652031706450ec6d3db176018cf6ede2523f668d
Darren Ricalton <dtrica17@stlawu.edu>
2020-01-29 14:16:47 -0500

    Fibonacci sequence

--- javascript/exercises.js
+++ javascript/exercises.js
@@ -11,7 +11,7 @@ console.log(`Total sum of squares: ${sum}`);
 // 2. Print the first Fibonacci number that is greater than 1000
 let n = 1
 let result = 1;
-while (result > 1000){
+while (result < 1000){
   let temp = result;
   result = result + n;
   n = temp;

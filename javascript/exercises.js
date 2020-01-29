e0035c367eda25ce3fafaab1e8b33adfa49a67b6
Darren Ricalton <dtrica17@stlawu.edu>
2020-01-29 13:57:47 -0500

    exercise 1

---
+++ javascript/exercises.js
@@ -0,0 +1,19 @@
+// Some sample exercises in Javascript
+
+// 1. Print the sum of the squares of the integers from 1 through 10
+int sum = 0;
+for (let x = 1; x <= 10; x++) {
+  console.log(x * x);
+  sum = sum + x * x;
+}
+console.log(sum);
+
+// 2. Print the first Fibonacci number that is greater than 1000
+
+
+
+// 3. Print the number of integers from 1 through 100 that are divisible by 3 or 5 but not both
+
+
+
+// 4. Print the smallest integer that is divisible by all the integers from 1 through 10

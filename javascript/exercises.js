63cb362e17d0232ea1660e8410c4d40c09ad4b08
Darren Ricalton <dtrica17@stlawu.edu>
2020-02-05 11:13:59 -0500

    exercise 4

--- javascript/exercises.js
+++ javascript/exercises.js
@@ -8,6 +8,7 @@ for (let x = 1; x <= 10; x++) {
 }
 console.log(`Total sum of squares: ${sum}`);
 
+
 // 2. Print the first Fibonacci number that is greater than 1000
 let n = 1
 let result = 1;
@@ -33,3 +34,10 @@ console.log(count);
 
 
 // 4. Print the smallest integer that is divisible by all the integers from 1 through 10
+let integers = [];
+for (let x = 0; x < 100000; x++){
+  if ((x % 1 === 0) & (x % 2 === 0) & (x % 3 === 0) & (x % 4 === 0) & (x % 5 === 0) & (x % 6 === 0) & (x % 7 === 0) & (x % 8 === 0) & (x % 9 === 0) & (x % 10 === 0)){
+    integers.push(x);
+  }
+}
+console.log(integers[0]);

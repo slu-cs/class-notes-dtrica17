35858e30c02081ab8c7cd9fd0b4bf60b9b6c8a25
Darren Ricalton <dtrica17@stlawu.edu>
2020-02-05 13:45:03 -0500

    JS console input

---
+++ javascript/input.js
@@ -0,0 +1,14 @@
+// Syntax for console and file input
+
+const readline = require('readline');
+
+// Console configuration
+const user = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+// Console input
+user.question('Filename: ', function(filename) {
+  console.log(filename);
+});

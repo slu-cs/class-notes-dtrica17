f9b8f37e507bfe1dc3b11a25ac2d828befab4a51
Darren Ricalton <dtrica17@stlawu.edu>
2020-02-05 14:22:51 -0500

    practice

--- javascript/callbackexercises.js
+++ javascript/callbackexercises.js
@@ -4,8 +4,8 @@
 // built-in filter function
 const numbers = [1, 2, 3, 4, 5];
 const odds = numbers.filter(x => x%2 > 0);
-console.log(odds); 
+console.log(odds);
 
 // example call
-//const numbers = [1, 2, 3, 4, 5];
-//const odds = filter(numbers, x => x%2 > 0);
+//const odds2 = filter(numbers, x => x%2 > 0);
+console.log(odds2);

bafe27a9e9c6b7aa4d78081e9bc6a7b8a6b5e4b9
Darren Ricalton <dtrica17@stlawu.edu>
2020-02-04 23:46:32 -0500

    typeof

--- javascript/quizpractice.js
+++ javascript/quizpractice.js
@@ -54,7 +54,7 @@ console.log(moby.check('Herman Melville')); // should be true
 // Define a function that works like the range function in Python
 let result = '[';
 const range = function(a, b) {
-  if (b !== undefined){
+  if (typeof b == 'undefined'){
     while (a < b) {
       result = result + a + ', ';
       a++;

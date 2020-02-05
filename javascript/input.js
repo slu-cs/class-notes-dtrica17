f2dcf6ab3be2ee5db8a4e2a5facf408511e5854c
Darren Ricalton <dtrica17@stlawu.edu>
2020-02-05 13:48:36 -0500

    Demonstrating asynchronous call

--- javascript/input.js
+++ javascript/input.js
@@ -12,3 +12,6 @@ const user = readline.createInterface({
 user.question('Filename: ', function(filename) {
   console.log(filename);
 });
+
+// This part isn't after the console input
+console.log('here');

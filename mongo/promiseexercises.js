b4f4b249ade9a9c3ce2ed234f744cf6cd84a56cc
Darren Ricalton <dtrica17@stlawu.edu>
2020-02-19 10:16:01 -0500

    js promise exercises

--- mongo/promiseexercises.js
+++ mongo/promiseexercises.js
@@ -7,18 +7,19 @@ f('hello', function(error, result) {
 
 // Promise code
 f('hello')
-  .then(result => console.log(result))
-  .catch(error => console.error(error.stack));
+.then(result => console.log(result))
+.catch(error => console.error(error.stack));
+
 
 // 2.
 // Suppose q1 and q2 are Mongoose queries
-
+// You want to run q1 and print the result, and afterwards run q2 and print the result
 // How can we do this without promises?
 q1.exec(function(error, r1) {
   if (error) console.log(error.stack);
   console.log(r1);
 
-  q2.exec(function(error, r2){
+  q2.exec(function(error, r2) {
     if (error) console.log(error.stack);
     console.log(r2);
   });
@@ -30,13 +31,14 @@ q1.exec()
     console.log(r1);
     return q2.exec();
   })
+
   .then(r2 => console.log(r2))
-  .catch(error => console.log(error.stack));
+  .catch(error => console.error(error.stack));
 
 // 3.
 // Suppose documents is an array of Mongoose documents that have been created but not saved.
 // You want to save them all at once (that is, in parallel). Write some code to do this. (simliar to how we did queries)
-const saves = documents.map(d => d.save());
+const saves = documents.map(d => d.save())
 Promise.all(saves)
   .then(() => console.log('All saved'))
-  .catch()
+  .catch(error => console.log(error.stack));

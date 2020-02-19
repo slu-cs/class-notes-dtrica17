f27dc5ad952ee04a05a6c8b22cbffd0a63a826be
Darren Ricalton <dtrica17@stlawu.edu>
2020-02-18 22:39:10 -0500

    test

--- javascript/callbackexercises.js
+++ javascript/callbackexercises.js
@@ -7,5 +7,17 @@ const odds = numbers.filter(x => x%2 > 0);
 console.log(odds);
 
 // example call
-//const odds2 = filter(numbers, x => x%2 > 0);
+/*
+const filter = function(array, transform) {
+  const copy = [];
+  for (const element of array) {
+    if (x % 2 > 0)
+    copy.push(transform(element));
+  }
+  return copy;
+}
+
+const odds2 = filter(numbers, x => x%2 > 0);
 console.log(odds2);
+
+*/

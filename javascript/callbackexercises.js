0f4c857358439bd47197291a6960b6e056c09442
Darren Ricalton <dtrica17@stlawu.edu>
2020-02-18 22:59:02 -0500

    number 3

--- javascript/callbackexercises.js
+++ javascript/callbackexercises.js
@@ -26,3 +26,30 @@ console.log(numbers2);
 
 numbers2.sort((a,b) => b - a);
 console.log(numbers2);
+
+// 3.
+
+// Read majors.csv (which needs to be on your VM when you run this code).
+const fs = require('fs');
+const readline = require('readline');
+const file = readline.createInterface({
+  input: fs.createReadStream('majors.csv')
+});
+
+// Create an array of objects, so that each line of the file is represented by an object with three properties
+const rows = [];
+file.on('line', function(line) {
+  const columns = line.split(',');
+  rows.push({
+    discipline: columns[0],
+    majors: Number(columns[1]),
+    minors: Number(columns[2])
+  });
+});
+
+
+// Then use the sort method (with a new arrow function) to sort disciplines by popularity. 
+// Let the popularity score for a discipline be the number of majors it has plus half the number of minors.
+file.on('close', function() {
+  rows.sort((a,b) => (a.majors + a.minors/2) - (b.majors + b.minors/2));
+});

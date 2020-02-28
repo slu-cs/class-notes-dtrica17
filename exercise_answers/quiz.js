0d009bfc939466f6fb0a0ddce322579b97f5fb08
Darren Ricalton <dtrica17@stlawu.edu>
2020-02-28 16:44:09 -0500

    test

--- exercise_answers/quiz.js
+++ exercise_answers/quiz.js
@@ -29,6 +29,14 @@ const numbers = [5, 4, 3, 2, 1];
 console.log(numbers.find(element => element % 2 === 0));
 
 // B. Define a standalone find function. The array is its first argument and the callback is its second argument.
+const filter = function(array, test) {
+  for (const element of array) {
+    if (test(element)) {
+      return element;
+    }
+  }
+};
+console.log(function(numbers, element => element % 2 === 0));
 
 
 //////////////////////////////////////////////////////////////// Question 3

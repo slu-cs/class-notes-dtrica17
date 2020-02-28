62cbad5aa72cb2283fd0027788fe2bee150795d5
Darren Ricalton <dtrica17@stlawu.edu>
2020-02-28 16:05:57 -0500

    test

--- exercise_answers/quiz.js
+++ exercise_answers/quiz.js
@@ -9,7 +9,7 @@ const labs = [
 ];
 
 // A. Generate a new array containing the seat numbers of all the labs (by invoking the built-in map method).
-const newt = labs.map(p -> p.seats);
+const newt = labs.map(p => p.seats);
 console.log(newt);
 
 // B. Generate a new array containing only the labs with more than 10 seats (by invoking the built-in filter method).

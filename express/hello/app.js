b368d0bf8209c578e5d2404b33a66c22633478bd
Darren Ricalton <dtrica17@stlawu.edu>
2020-03-04 13:35:25 -0500

    Express routes

--- express/hello/app.js
+++ express/hello/app.js
@@ -4,9 +4,29 @@ const express = require('express');
 // Create the server
 const app = express();
 
-// Respond to one request
+// http://cs-linuxlab-05.stlawu.edu:3000/
 app.get('/', function(request, response) {
   response.send('Hello Darren');
+});
+
+// http://cs-linuxlab-05.stlawu.edu:3000/foo
+app.get('/foo', function(request, response) {
+  response.send('Hello foo');
+});
+
+// http://cs-linuxlab-05.stlawu.edu:3000/foo/bar
+app.get('/foo/bar', function(request, response) {
+  response.send('Hello bar');
+});
+
+// http://cs-linuxlab-05.stlawu.edu:3000/zap/z (for any z)
+app.get('/zap/:z', function(request, response) {
+  response.send(`Hello ${request.params.z}`);
+});
+
+// http://cs-linuxlab-05.stlawu.edu:3000/zap?x=&y= (for any z)
+app.get('/zap', function(request, response) {
+  response.send(`Hello zap with ${request.query.x} and ${request.query.y}`);
 });
 
 // Start the server

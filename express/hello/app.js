90025c7e21ee3c29e973f9cc15895fa427af7045
Darren Ricalton <dtrica17@stlawu.edu>
2020-03-04 13:55:12 -0500

    Express middleware setup

--- express/hello/app.js
+++ express/hello/app.js
@@ -24,7 +24,7 @@ app.get('/zap/:z', function(request, response) {
   response.send(`Hello ${request.params.z}`);
 });
 
-// http://cs-linuxlab-05.stlawu.edu:3000/zap?x=&y= (for any z)
+// http://cs-linuxlab-05.stlawu.edu:3000/zap?x=X&y=Y (for any X and Y)
 app.get('/zap', function(request, response) {
   response.send(`Hello zap with ${request.query.x} and ${request.query.y}`);
 });

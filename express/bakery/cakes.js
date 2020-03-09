9800c88f64856d384a202716a69e72940864270a
Darren Ricalton <dtrica17@stlawu.edu>
2020-03-09 14:03:50 -0400

    update

--- express/bakery/cakes.js
+++ express/bakery/cakes.js
@@ -7,32 +7,26 @@ const router = express.Router();
 
 // Pretend database collection
 const cakes = [
-{id: 'vanilla', flavor: 'Vanilla'},
-{id: 'chocolate', flavor: 'Chocolate'}
+  {id: 'vanilla', flavor: 'Vanilla'},
+  {id: 'chocolate', flavor: 'Chocolate'}
 ];
 
 // cs-linuxlab-##.stlawu.edu:3000/cakes/
 router.get('/', function(request, response) {
-response.send(`
-  <h1>Cakes</h1>
-  <ul>
-    <li><a href="/cakes/vanilla">Vanilla</a></li>
-    <li><a href="/cakes/chocolate">Chocolate</a></li>
-  </ul>
-`);
+  response.render('cakes/index', {cakes: cakes});
 });
 
 // cs-linuxlab-##.stlawu.edu:3000/cakes/id
 router.get('/:id', function(request, response, next) {
 
-// Pretend database lookup
-const cake = cakes.find(cake => cake.id === request.params.id);
+  // Pretend database lookup
+  const cake = cakes.find(cake => cake.id === request.params.id);
 
-if (!cake) {
-  next(); // Pass on this request
-} else {
-  response.render('cakes/detail', {cake: cake});
-}
+  if (!cake) {
+    next(); // Pass on this request
+  } else {
+    response.render('cakes/detail', {cake: cake});
+  }
 });
 
 module.exports = router;
